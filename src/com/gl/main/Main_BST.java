d951369e4898a63f1aea825d77594d6916f4b25e
Saatvat <saatvatg@gmail.com>
2022-05-16 03:56:29 +0530

    few corrections

--- src/com/gl/main/Main_BST.java
+++ src/com/gl/main/Main_BST.java
@@ -1,10 +1,18 @@
 package com.gl.main;
+import com.gl.model.RightSkewed;
 
 public class Main_BST {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main (String[] args)
+	{
+		RightSkewed tree = new RightSkewed();
+		tree.node = tree.newNode(30);
+		tree.node.left = tree.newNode(20);
+        tree.node.right = tree.newNode(60);
+        tree.node.left.left = tree.newNode(10);
+        tree.node.right.left = tree.newNode(55);
+        
+		tree.rightskew(tree.node);
+		tree.traverseRightSkewed(tree.headNode);
 	}
-
 }
--- src/com/gl/model/RightSkewed.java
+++ src/com/gl/model/RightSkewed.java
@@ -2,4 +2,46 @@ package com.gl.model;
 
 public class RightSkewed {
 
-}
+	public class Node {
+		public int key;
+		public Node left, right;
+	}
+
+	public Node newNode(int data) {
+		Node temp = new Node();
+		temp.key = data;
+		temp.left = null;
+		temp.right = null;
+		
+		return temp;
+		}
+	
+	public Node node, prevNode = null, headNode = null;
+	
+		public void rightskew(Node root) {
+			if(root == null) {
+				return;
+			}
+				rightskew(root.left);
+			
+			if (headNode == null) {
+				headNode = root;
+				root.left = null;
+				prevNode = root;
+			}
+			else {
+				prevNode.right = root;
+				root.left = null;
+				prevNode = root;
+			}
+				rightskew(root.right);
+		}
+
+		public void traverseRightSkewed(Node root) {
+			if(root == null) {
+				return;
+			}
+			System.out.print(root.key + " ");
+			traverseRightSkewed(root.right);	
+			}
+		}
